887757073f54fe85a8b2a8372fc885f0d7f2b297
Sofistication <sordonez93@gmail.com>
2017-01-23 12:54:37 -0500

    Commit class work
    -just committing work from class

--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#curl "http://localhost:3000/sign-up" \
-curl "http://httpbin.org/post" \
+#"credentials%5Bemail%5D=jeff%40jrhorn.me&credentials%5Bpassword%5D=supersekret&credentials%5Bpassword_confirmation%5D=supersekret"
+curl ${API}${API_PATH} \
   --include \
   --request POST \
-  --data-urlencode ""
+  --data-urlencode ${DATA}
 
 # --header "Content-Type: application/x-www-form-urlencoded"
 
